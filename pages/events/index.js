98a30a9abee802fd0be0a46af9b37a02868ae174
Douglas Post <88281271+DouglasPost@users.noreply.github.com>
2022-05-24 11:27:30 -0600

    Added navbar and browse all events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+
 function AllEventsPage() {
+  const allEvents = getAllEvents();
+
   return (
     <div>
-      <h1>All Events</h1>
+      <EventList items={allEvents} />
     </div>
   );
 }
